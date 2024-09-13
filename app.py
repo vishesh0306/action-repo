b77ede55edda5d61875fcce0cb6827611fe75e0a
vishesh0306 <115298750+vishesh0306@users.noreply.github.com>
2024-09-13 19:05:27 +0530

    app.py created

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route for the homepage
+@app.route('/')
+def home():
+    return "Hello, Flask is running!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
